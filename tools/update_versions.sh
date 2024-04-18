4645286715c1af0a72fcc040ec27ac7a5d2c425e
David R Connell <david32@dcon.addy.io>
2024-04-18 13:24:50 -0500

    Enclose all generated files into src/build
    - To keep them together instead of having to track and explicitly remove
    generated files.
    - Move the *_VERSION files.
    - Make cmake use R config variables.

--- tools/update_versions.sh
+++ tools/update_versions.sh
@@ -1,15 +1,14 @@
-ROOT=$(
-    cd $(dirname $0)
-    cd ..
+TOOLS=$(
+    cd "$(dirname "$0")"
     pwd
 )
-SE2="${ROOT}/tools/se2"
+SE2="${TOOLS}/se2"
 IGRAPH="${SE2}/vendor/igraph"
 
 # Cmake usually gets version from git but git directory is not available at
 # build time, need to create the version files to prevent cmake from failing.
-cd "$IGRAPH"
-git describe --tags >${ROOT}/IGRAPH_VERSION
+cd "$IGRAPH" || exit
+git describe --tags >"${TOOLS}/IGRAPH_VERSION"
 
-cd "$SE2"
-git describe --tags >${ROOT}/SE2_VERSION
+cd "$SE2" || exit
+git describe --tags >"${TOOLS}/SE2_VERSION"
