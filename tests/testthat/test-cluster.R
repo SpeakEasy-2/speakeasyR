4a99a3af6bf947fa616af7d90560cba670d76231
David R Connell <david32@dcon.addy.io>
2024-08-18 14:21:10 -0500

    Update libspeakeasy2

--- tests/testthat/test-cluster.R
+++ tests/testthat/test-cluster.R
@@ -2,8 +2,8 @@ if (require("igraph")) {
   seed <- 1234
   graph <- igraph::graph.famous("zachary")
   expected_membership <- c(
-    3, 3, 3, 3, 4, 4, 4, 3, 1, 8, 4, 2, 3, 3, 6, 6, 4, 3, 6, 3, 6, 3, 6, 9,
-    7, 7, 9, 9, 5, 9, 1, 7, 6, 6
+    9, 9, 9, 9, 1, 1, 1, 9, 3, 8, 1, 2, 9, 9, 5, 5, 1, 9, 5, 9, 5, 9, 5, 6, 7,
+    7, 6, 6, 4, 6, 3, 7, 5, 5
   )
 
   test_that("matrix works", {
